299c02ce78d3920792cd7ae6915b7ee346e4b9c6
Carl Hain <crhain@gmail.com>
2019-04-06 19:02:43 -0500

    Write fetchTopIds method for NewsApiProvider class

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,9 +1,16 @@
 import 'package:http/http.dart' show Client;
+import 'dart:convert';
 
 class NewsApiProvider {
   Client client = Client();
 
-  fetchTopIds() {}
+  fetchTopIds() async {
+    final response = await client
+        .get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    final ids = json.decode(response.body);
+
+    return ids;
+  }
 
   fetchItem() {}
 }
