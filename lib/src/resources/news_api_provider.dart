53e6ac1284790b23801fbdc0da4fe9beb0bc9aa3
Carl Hain <crhain@gmail.com>
2019-04-07 19:13:32 -0500

    Refactor news_api_provider.dart file to declare root of url used for fetching ids and items as a constant

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -2,20 +2,20 @@ import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import '../models/item_model.dart';
 
+final _root = 'https://hacker-news.firebaseio.com/v0';
+
 class NewsApiProvider {
   Client client = Client();
 
   fetchTopIds() async {
-    final response = await client
-        .get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    final response = await client.get('$_root/topstories.json');
     final ids = json.decode(response.body);
 
     return ids;
   }
 
   fetchItem(int id) async {
-    final response =
-        await client.get('https://hacker-news.firebaseio.com/v0/item/$id.json');
+    final response = await client.get('$_root/item/$id.json');
 
     final item = ItemModel.fromJson(json.decode(response.body));
 
