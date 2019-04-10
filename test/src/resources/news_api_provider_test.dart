aa9c3b581eda1c18e92add654f9062b521d85062
Carl Hain <crhain@gmail.com>
2019-04-09 22:58:07 -0500

    set newsApi object's client field to a mock that returns a fake list of ids

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -7,5 +7,10 @@ import 'package:http/testing.dart';
 void main() {
   test('FetchTopIds returns a list of ids', () {
     final newsApi = NewsApiProvider();
+
+    //create a mock client that returns some fake list of ids
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
   });
 }
