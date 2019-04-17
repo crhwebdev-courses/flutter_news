d23622c5f3820c1ff45983c79b3eade9e6029aa9
Carl Hain <crhain@gmail.com>
2019-04-17 15:26:14 -0500

    Revert NewsList build method back to just displaying some text on the screen

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -7,34 +6,7 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
-    );
-  }
-
-  //use ListView.builder to render items to screen only if they are visible
-  // FutureBuilder will resolve each future it recieves as it recieves it
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return Container(
-                height: 80.0,
-                child: snapshot.hasData
-                    ? Text('I\'m visible $index')
-                    : Text('I haven\'t fetched data yet'));
-          },
-        );
-      },
-    );
-  }
-
-  Future getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'hi',
+      body: Text('Show a list'),
     );
   }
 }
