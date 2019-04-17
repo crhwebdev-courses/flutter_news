54879b54777f767986308f7b02d9b23f28b5c736
Carl Hain <crhain@gmail.com>
2019-04-17 15:17:42 -0500

    Create test version of NewsList that fetches fake items and displays them on the screen

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -6,7 +7,32 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show some news here!'),
+      body: buildList(),
+    );
+  }
+
+  //use ListView.builder to render items to screen only if they are visible
+  // FutureBuilder will resolve each future it recieves as it recieves it
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, index) {
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? Text('I\'m visible $index')
+                : Text('I haven\'t fetched data yet');
+          },
+        );
+      },
+    );
+  }
+
+  Future getFuture() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => 'hi',
     );
   }
 }
