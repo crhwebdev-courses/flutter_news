2063dd103544bc5b2841690100d628b899fce7e7
Carl Hain <crhain@gmail.com>
2019-04-18 12:38:30 -0500

    Create basic StoriesBlock class with a getter for topIds and a dispose method

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,1 +1,15 @@
-class StoriesBloc {}
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class StoriesBloc {
+  final _topIds = PublishSubject<List<int>>();
+
+  // Getters to get Streams
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  //destructor
+  dispose() {
+    _topIds.close();
+  }
+}
