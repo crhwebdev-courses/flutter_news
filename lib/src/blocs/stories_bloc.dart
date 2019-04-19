59e60e1eab6bb0c10946c8e5089b2be49357789c
Carl Hain <crhain@gmail.com>
2019-04-19 11:40:00 -0500

    Add fetchTopIds method to StoriesBlock

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -3,10 +3,20 @@ import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class StoriesBloc {
+  final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
 
   // Getters to get Streams
   Observable<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    //fetch ids from repository
+    final ids = await _repository.fetchTopIds();
+
+    //add ids to _topIds sink so that they are available to other
+    // components
+    _topIds.sink.add(ids);
+  }
 
   //destructor
   dispose() {
