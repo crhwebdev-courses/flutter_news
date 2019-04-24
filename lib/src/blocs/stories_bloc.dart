81439090846577abd6b101e10e0d16735b4da369
Carl Hain <crhain@gmail.com>
2019-04-24 13:15:40 -0500

    Rename _items instance variable in StoriesBloc to _itemsOutput and change it's type to refelect new function

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -39,7 +39,7 @@ class StoriesBloc {
   //for each id given to it
   _itemsTransformer() {
     return ScanStreamTransformer(
-      (Map<int, Future<ItemModel>> cache, int id, _) {
+      (Map<int, Future<ItemModel>> cache, int id, index) {
         cache[id] = _repository.fetchItem(id);
         return cache;
       },
