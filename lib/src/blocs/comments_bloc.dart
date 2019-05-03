e9fde537e8b908269307439c2048272337a1984f
Carl Hain <crhain@gmail.com>
2019-05-03 17:41:23 -0500

    Add private instance variables for a PublishSubject and BehaviorSubject to CommentsBlock; add dispose method to close those two streams when the object is remoeved

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -3,4 +3,12 @@ import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
 
-class CommentsBloc {}
+class CommentsBloc {
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
